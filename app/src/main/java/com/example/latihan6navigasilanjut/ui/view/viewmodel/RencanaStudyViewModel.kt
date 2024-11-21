adc246881b2e804ff8b40598cfc6c1b5359ffe18
LYChubby <f4uzi345@gmail.com>
2024-11-22 04:50:43 +0700

    Membuat Fungsi saveDataKRS

--- app/src/main/java/com/example/latihan6navigasilanjut/ui/view/viewmodel/RencanaStudyViewModel.kt
+++ app/src/main/java/com/example/latihan6navigasilanjut/ui/view/viewmodel/RencanaStudyViewModel.kt
@@ -17,6 +17,11 @@ class RencanaStudyViewModel : ViewModel() {
 
     fun setKelas(kelasPilihan: String) {
         _krsState.update { stateKelas -> stateKelas.copy(kelas = kelasPilihan) }
+    }
 
+    fun saveDataKRS(ls: MutableList<String>) {
+        _krsState.update { status -> status.copy(
+            mataKuliah = ls[0],
+            kelas = ls[1]) }
     }
 }
