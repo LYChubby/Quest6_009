a243cf67f64c7b856e365e0de401f44196173d16
LYChubby <f4uzi345@gmail.com>
2024-11-22 05:15:27 +0700

    Membuat Class MahasiswaViewModel dan Membuat Private dan public Variabel

--- app/src/main/java/com/example/latihan6navigasilanjut/ui/view/viewmodel/MahasiswaViewModel.kt
+++ app/src/main/java/com/example/latihan6navigasilanjut/ui/view/viewmodel/MahasiswaViewModel.kt
@@ -1,4 +1,15 @@
 package com.example.latihan6navigasilanjut.ui.view.viewmodel
 
-class MahasiswaViewModel {
+import androidx.lifecycle.ViewModel
+import com.example.latihan6navigasilanjut.model.Mahasiswa
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class MahasiswaViewModel : ViewModel() {
+    private val _mahasiswaState = MutableStateFlow(Mahasiswa())
+    val mahasiswaStateUi : StateFlow<Mahasiswa> = _mahasiswaState.asStateFlow()
+
+
 }
