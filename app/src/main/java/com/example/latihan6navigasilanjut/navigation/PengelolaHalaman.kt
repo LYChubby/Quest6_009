c30b57742818d20001cd840768246bd29f4561ad
LYChubby <f4uzi345@gmail.com>
2024-11-22 05:47:00 +0700

    Membuat NavHost

--- app/src/main/java/com/example/latihan6navigasilanjut/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/latihan6navigasilanjut/navigation/PengelolaHalaman.kt
@@ -1,10 +1,13 @@
 package com.example.latihan6navigasilanjut.navigation
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
 import com.example.latihan6navigasilanjut.ui.view.viewmodel.MahasiswaViewModel
 import com.example.latihan6navigasilanjut.ui.view.viewmodel.RencanaStudyViewModel
 
@@ -18,9 +21,16 @@ enum class Halaman {
 @Composable
 fun PengelolaHalaman(
     modifier: Modifier = Modifier,
-    viewModel: MahasiswaViewModel = viewModel(),
-    viewModelMK: RencanaStudyViewModel = viewModel(),
+    mahasiswaViewModel: MahasiswaViewModel = viewModel(),
+    krsViewModel: RencanaStudyViewModel = viewModel(),
     navController: NavHostController = rememberNavController()
 ) {
+    val mahasiswaUiState = mahasiswaViewModel.mahasiswaStateUi.collectAsState().value
+    NavHost(
+        navController = navController,
+        startDestination = Halaman.Splash.name,
+        modifier = modifier.padding()
+    ) {
 
+    }
 }
