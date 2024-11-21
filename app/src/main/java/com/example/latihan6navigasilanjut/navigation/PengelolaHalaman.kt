8d13d911ce0f61bf0b050be836b1296c73b67099
LYChubby <f4uzi345@gmail.com>
2024-11-22 05:41:02 +0700

    Membuat Fungsi PengelolaHalaman dan Penyesuaian dependensi pada gradle

--- app/src/main/java/com/example/latihan6navigasilanjut/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/latihan6navigasilanjut/navigation/PengelolaHalaman.kt
@@ -1,8 +1,26 @@
 package com.example.latihan6navigasilanjut.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.latihan6navigasilanjut.ui.view.viewmodel.MahasiswaViewModel
+import com.example.latihan6navigasilanjut.ui.view.viewmodel.RencanaStudyViewModel
+
 enum class Halaman {
     Splash,
     Mahasiswa,
     Matakuliah,
     Tampil
+}
+
+@Composable
+fun PengelolaHalaman(
+    modifier: Modifier = Modifier,
+    viewModel: MahasiswaViewModel = viewModel(),
+    viewModelMK: RencanaStudyViewModel = viewModel(),
+    navController: NavHostController = rememberNavController()
+) {
+
 }
